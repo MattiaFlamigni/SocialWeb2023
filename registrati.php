7a521269d73e4ed434178f336a6e87a65ef19a35
Mattia Flamigni <mattia.flamigni4@studio.unibo.it>
2023-12-29 02:52:57 +0100

    set up php for registration page

---
+++ registrati.php
@@ -0,0 +1,11 @@
+<?php
+require_once("bootstrap.php");
+
+$templateParams["titolo"] = "Registrati";
+$templateParams["titolo_pagina"] = "Registrati";
+$templateParams["nome"] = "registrati.php";
+$templateParams["nascondi_footer"] = true;
+
+require("template/base.php");
+
+?>
