9eb05b2f2385d559458a8857770f4d521fd7e5e4
Edoardo La Greca <edo@edolg.it>
2023-12-30 20:08:45 +0100

    rename image_path function to image_url

--- util/images.php
+++ util/images.php
@@ -1,8 +1,8 @@
 <?php
 $imgdir = '' // TODO
 
-function image_path($id) {
-	$filedir = $imgdir . '$id.png'
+function image_url($id) {
+	$filedir = $imgdir . "$id.png"
 	// TODO: do not use full paths as it may expose sensitive data
 	return $filedir
 }
