af8cdc741ff8b0bdff512cb10fc44cece4de8452
Mattia Flamigni <mattia.flamigni4@studio.unibo.it>
2023-12-31 02:26:25 +0100

    add ;

--- db/post.php
+++ db/post.php
@@ -1,18 +1,18 @@
 <?php
 
 class Post {
-	public $url // post image URL
-	public $user
-	public $user_picture // URL to user's profile picture
-	public $description
-	public $date
+	public $url; // post image URL
+	public $user;
+	public $user_picture; // URL to user's profile picture
+	public $description;
+	public $date;
 
 	public function __construct($url, $user, $user_pic, $description, $date) {
-		$this->url = $url
-		$this->user = $user
-		$this->user_pic = $user_pic
-		$this->description = $description
-		$this->date = $date
+		$this->url = $url;
+		$this->user = $user;
+		$this->user_pic = $user_pic;
+		$this->description = $description;
+		$this->date = $date;
 	}
 }
 
