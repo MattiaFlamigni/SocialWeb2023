18e2966369060550a4bef3d9ed3be3190d922aee
Mattia Flamigni <mattia.flamigni4@studio.unibo.it>
2024-01-01 13:16:44 +0100

    created separate class for sending mail function

--- util/registrazione.php
+++ util/registrazione.php
@@ -2,16 +2,7 @@
 
 require_once('../db/database.php');
 require_once("../bootstrap.php");
-
-require '..\PHPMailer\src/PHPMailer.php';
-require '..\PHPMailer\src\Exception.php';
-require '..\PHPMailer\src\SMTP.php';
-
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\Exception;
-use PHPMailer\PHPMailer\SMTP;
-
-
+require_once("./Mailer.php");
 
     echo "Registrazione in corso...<br>";
     $username = $_POST["username"];
@@ -26,53 +17,10 @@ use PHPMailer\PHPMailer\SMTP;
         header("Refresh:0; url=../index.php");
         echo "Verrai reindirizzato alla pagina di login tra 5 secondi. Se non vuoi attendere <a href='registrazione.html'>clicca qui</a>";
     } else {    
+        $mailer = new Mailer();
+        $mailer->sendMail("flami2002@gmail.com", $email, "Registrazione avvenuta con successo!", "Ciao $nome, la tua registrazione è avvenuta con successo!");
 
-
-        /*TODO: FILE SEPARATO*/
-        ini_set('SMTP','sandbox.smtp.mailtrap.io');
-        ini_set('smtp_port',2525);
-
-
-
-        $phpmailer = new PHPMailer();
-        $phpmailer->isSMTP();
-        $phpmailer->Host = 'sandbox.smtp.mailtrap.io';
-        $phpmailer->SMTPAuth = true;
-        $phpmailer->Port = 2525;
-        $phpmailer->Username = '539659f18e12f5';
-        $phpmailer->Password = '3a718da697e70d';
-
-
-        $phpmailer->setFrom('flami2002@gmail.com', 'Mailer');
-        $phpmailer->addAddress($email);
-        $phpmailer->Subject = 'Here is the subject';
-        $phpmailer->Body = 'This is the HTML message body <b>in bold!</b>';
-        $phpmailer->AltBody = 'This is the body in plain text for non-HTML mail clients';
-
-        if(!$phpmailer->send()) {
-            echo 'Mailer Error: ' . $phpmailer->ErrorInfo;
-        } else {
-            echo 'Message has been sent';
-        }
-
-        
-
-
-
-
-        /*$to = 'flami2002@gmail.com ';
-        $subject = 'Test Email';
-        $message = 'Hello, this is a test email.';
-
-        mail($to, $subject, $message);*/
-
-
-
-
-
-
-
-        //header("Refresh:0; url=../index.php");
+        header("Refresh:0; url=../index.php");
         echo '<script>alert("Registrazione avvenuta con successo!");</script>';
     }
 
