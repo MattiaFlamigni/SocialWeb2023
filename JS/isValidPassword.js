43c49280be5d211e037fd944bcc0cd05408515c1
Mattia Flamigni <mattia.flamigni4@studio.unibo.it>
2024-01-02 12:10:10 +0100

    created separate javascript for check password, mail and user

---
+++ JS/isValidPassword.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", function() {
+    let passwordInput = document.getElementById("password");
+    let mail = document.getElementById("mail");
+
+    passwordInput.addEventListener("input", function() {
+        let password = document.getElementById("password").value;
+        let passwordResult = document.getElementById("password-check-result");
+
+        // La password deve contenere almeno un carattere maiuscolo e almeno un numero
+        if (!/[A-Z]/.test(password) || !/\d/.test(password) || password.length < 8) {
+            passwordResult.innerHTML = "La password deve contenere un <strong>carattere maiuscolo</strong>, un <strong>numero</strong> e una <strong>lunghezza</strong> superiore a <strong>8</strong>.";
+            //document.getElementById("registrazioneBtn").disabled = false;
+        } else {
+            passwordResult.innerHTML = "";  // Resetta il messaggio se la password è valida
+            //document.getElementById("registrazioneBtn").classList.remove("disabled");
+            
+            
+        }
+    });
+});
---
+++ JS/isValidUser.js
@@ -0,0 +1,29 @@
+document.getElementById("username").addEventListener("input", function() {
+    var username = this.value;
+    var resultContainer = document.getElementById("username-check-result");
+
+    // Esegui la verifica solo se l'input è lungo almeno 3 caratteri
+    if (username.length >= 3) {
+        // Esegui una richiesta AJAX per verificare l'username
+        var xhr = new XMLHttpRequest();
+        xhr.open("POST", "check_username.php", true);
+        xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState == 4 && xhr.status == 200) {
+                // Gestisci la risposta ricevuta
+                resultContainer.innerHTML = xhr.responseText;
+
+                // Disabilita il bottone se l'username è già in uso
+              if (xhr.responseText.includes("in uso")) {
+                  document.getElementById("registrazioneBtn").classList.add("disabled");
+              } else {
+                  
+              }
+            }
+        };
+        xhr.send("username=" + username);
+    } else {
+        // Se l'input è troppo breve, svuota il risultato
+        resultContainer.innerHTML = "";
+    }
+});
