ae4eebca7b5f7d55cc98f08db4accb2b62ab610b
Mattia Flamigni <mattia.flamigni4@studio.unibo.it>
2024-01-02 12:43:24 +0100

    minor improvements

--- util/recover-username.php
+++ util/recover-username.php
@@ -11,8 +11,11 @@ $result = $dbh->recoverUser($mail, $password);
 if($result==true){
     $mailer = new Mailer();
     $mailer->sendMail("flami2002@gmail.com", $mail, "Recupero username", "Il tuo username è:" .  $result);
+    echo '<script>alert("Email inviata con successo!");</script>';
+    header("Refresh:0; url=../index.php");
 }else{
-    '<script>alert("Utente non trovato!");</script>';
+    echo '<script>alert("Utente non trovato!");</script>';
+    header("Refresh:0; url=../index.php");
 }
 
 
