2d1b696921afa4b80bff7c122e4777eb46b5f8b9
Edoardo La Greca <edo@edolg.it>
2024-01-03 19:53:19 +0100

    remove duplicated bootstrap and font awesome links

--- template/base.php
+++ template/base.php
@@ -5,14 +5,10 @@
             <meta name="viewport" content="width=device-width, initial-scale=1">
            
             <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
             <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" integrity="sha384-GLhlTQ8iK16H3StjBvfxqSBXAm12XtwW1l75Vg5WBE5iP5S62XVtFkF+bpXQtXWo" crossorigin="anonymous">
             <script src="https://kit.fontawesome.com/3e59e20392.js" crossorigin="anonymous"></script>
-            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css"
-                integrity="sha384-GLhlTQ8iK16H3StjBvfxqSBXAm12XtwW1l75Vg5WBE5iP5S62XVtFkF+bpXQtXWo" crossorigin="anonymous">
-            <script src="https://kit.fontawesome.com/3e59e20392.js" crossorigin="anonymous"></script>
             <link href="CSS/home.css" rel="stylesheet" type="text/css">
-            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 
             <script src="JS/timeOutLogout.js"></script>
             
